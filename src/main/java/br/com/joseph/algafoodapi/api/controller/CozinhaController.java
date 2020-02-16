2d3f149255bc3c29575177e91288eee2289d37b6
Joseph <joseluis.bsa@gmail.com>
2020-02-16 10:33:41 -0300

    wip

---
+++ src/main/java/br/com/joseph/algafoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,63 @@
+package br.com.joseph.algafoodapi.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joseph.algafoodapi.domain.model.Cozinha;
+import br.com.joseph.algafoodapi.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.listar();
+	}
+
+	@GetMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+		if(cozinhaAtual != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+			cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
-INSERT INTO tb_cozinha (nome) values ('Tailandia');
-INSERT INTO tb_cozinha (nome) values ('Indiana');
+INSERT INTO tb_cozinha (id, nome) values (1, 'Tailandia');
+INSERT INTO tb_cozinha (id, nome) values (2, 'Indiana');
+
+INSERT INTO tb_restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
+INSERT INTO tb_restaurante (nome, taxa_frete, cozinha_id) values ('Cho Burguer', 9.5, 1);
+INSERT INTO tb_restaurante (nome, taxa_frete, cozinha_id) values ('Tuk tk food', 15, 2);
